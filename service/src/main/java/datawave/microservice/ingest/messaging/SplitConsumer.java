afc263fc23ab2634ed13fd875ef35f0a7c10a9c6
Charles Williams <cwilliams.work@gmail.com>
2021-12-02 18:46:29 +0000

    fix dependencies, .gitignore, and annotations

--- service/src/main/java/datawave/microservice/ingest/messaging/SplitConsumer.java
+++ service/src/main/java/datawave/microservice/ingest/messaging/SplitConsumer.java
@@ -2,14 +2,12 @@ package datawave.microservice.ingest.messaging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
 @Configuration
-@EnableConfigurationProperties(SplitConsumer.class)
 public class SplitConsumer {
     private Logger log = LoggerFactory.getLogger(this.getClass());
     
