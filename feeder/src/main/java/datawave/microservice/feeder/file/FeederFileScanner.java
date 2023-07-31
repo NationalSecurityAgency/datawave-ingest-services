b5be6af3a810dd57d9fd9cd20f51775d58034c58
Connor Koehler <cfkoehler.work@gmail.com>
2023-07-31 17:39:26 +0000

    Small cleanup in FeederFileScanner

--- feeder/src/main/java/datawave/microservice/feeder/file/FeederFileScanner.java
+++ feeder/src/main/java/datawave/microservice/feeder/file/FeederFileScanner.java
@@ -1,8 +1,5 @@
 package datawave.microservice.feeder.file;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
 import datawave.microservice.feeder.configuration.FeederProperties;
 import datawave.microservice.feeder.messaging.MessageSupplier;
 import datawave.microservice.file.FileScanner;
@@ -15,9 +12,6 @@ import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Feeder will run on ${file.frequency} interval. Files from the inputDir will be scanned and moved to the targetDir, preserving path beyond the inputDir if
@@ -27,8 +21,7 @@ import java.util.concurrent.TimeUnit;
 public class FeederFileScanner extends FileScanner {
     private final FeederProperties feederProperties;
     private final MessageSupplier feedSource;
-    private Cache<Path,Boolean> failureCache;
-    
+
     @Autowired
     public FeederFileScanner(Configuration conf, FileScannerProperties fileScannerProperties, FeederProperties feederProperties, MessageSupplier feedSource) {
         super(conf, fileScannerProperties);
@@ -39,7 +32,7 @@ public class FeederFileScanner extends FileScanner {
     @Override
     protected void process(Path workingFile) throws IOException {
         Path targetDir = new Path(feederProperties.getTargetDir());
-        log.info("would have sent message: " + workingFile + "," + feederProperties.getInputFormatClass() + "," + feederProperties.getDataType());
+        log.info("would have sent message: {},{},{}", workingFile,feederProperties.getInputFormatClass(),feederProperties.getDataType());
         
         Path fileTarget = new Path(targetDir, workingFile.getName());
         if (feederProperties.isPreservePath()) {
@@ -52,12 +45,12 @@ public class FeederFileScanner extends FileScanner {
             }
         }
         
-        log.info("Moving file: " + workingFile + " to: " + fileTarget);
+        log.info("Moving file: {} to: {}", workingFile, fileTarget);
         FileSystem fs = FileSystem.get(workingFile.toUri(), conf);
         Path parentDir = fileTarget.getParent();
         if (!fs.exists(parentDir)) {
             if (!fs.mkdirs(parentDir)) {
-                log.warn("Failed to create " + parentDir);
+                log.warn("Failed to create {}", parentDir);
                 throw new IOException("Failed to create " + parentDir);
             }
         }
@@ -73,7 +66,7 @@ public class FeederFileScanner extends FileScanner {
                 }
             }
         } else {
-            log.warn("Failed to rename " + workingFile + " to " + fileTarget);
+            log.warn("Failed to rename {} to {}", workingFile, fileTarget);
             throw new IOException("Failed to rename " + workingFile + " to " + fileTarget);
         }
     }
